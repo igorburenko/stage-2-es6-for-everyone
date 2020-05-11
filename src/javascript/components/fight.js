07dccb5f93b56cc360141c3109f5280fe1a66b1e
Ihor Burenko <burenko@gmail.com>
2020-05-11 23:37:53 +0300

    feat: add all functionality

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -2,18 +2,83 @@ import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
-    // resolve the promise with the winner when fight is over
+    fighters.setFighters(firstFighter, secondFighter);
+    document.addEventListener('keydown', (event) => {
+      onKeyDown(event, resolve);
+    });
+    document.addEventListener('keyup', (event) => {
+      onKeyUp(event);
+    });
   });
 }
 
+const fighters = {
+  setFighters(firstFighter, secondFighter) {
+    this.first = firstFighter;
+    this.first.maxHealth = parseInt(`${this.first.health}`);
+    this.second = secondFighter;
+    this.second.maxHealth = parseInt(`${this.second.health}`);
+  }
+};
+
+export function onKeyDown(key, resolve) {
+  setBlock(key.code, true);
+  if (key.code === controls.PlayerOneAttack) {
+    fighters.second.health -= getDamage(fighters.first, fighters.second);
+    renderHealthIndicator('left-fighter-indicator', fighters.second);
+  } else if (key.code === controls.PlayerTwoAttack) {
+    fighters.first.health -= getDamage(fighters.second, fighters.first);
+    renderHealthIndicator('right-fighter-indicator', fighters.first)
+  }
+  if (isGameOver()) {
+    resolve(fighters.first.health <= 0 ? fighters.second : fighters.first);
+  }
+}
+
+function renderHealthIndicator(indicatorId, fighter) {
+  const indicator = document.querySelector(`#${indicatorId}`);
+  console.log(fighter.health);
+  indicator.style.width = `${fighter.health / fighter.maxHealth * 100}%`
+}
+
+export function isGameOver() {
+  return fighters.first.health <= 0 || fighters.second.health <= 0;
+}
+
+export function onKeyUp(key) {
+  setBlock(key.code, false)
+}
+
+function setBlock(key, isBlocked) {
+  if (key === controls.PlayerOneBlock) {
+    fighters.first.block = isBlocked;
+  }
+  if (key === controls.PlayerTwoBlock) {
+    fighters.second.block = isBlocked;
+  }
+}
+
 export function getDamage(attacker, defender) {
-  // return damage
+  if (attacker.block) return 0;
+  const damage = defender.block ? getHitPower(attacker) - getBlockPower(defender) : getHitPower(attacker);
+  const misfire = 0;
+  return damage > misfire ? damage : 0;
 }
 
 export function getHitPower(fighter) {
-  // return hit power
+  return fighter.attack * criticalHitDamageChance();
 }
 
 export function getBlockPower(fighter) {
-  // return block power
+  return fighter.defense * criticalHitDamageChance();
 }
+
+export function criticalHitDamageChance() {
+  return Math.random() + 1;
+}
+
+// TODO: Бойцы также могут наносить критические удары,
+//  которые не могут быть заблокированы и вычисляются по формуле 2 * attack,
+//  где attack - характеристика бойца.
+//  Для того, чтобы бойцу нанести такой удар, нужно одновременно нажать 3 соответствующие клавиши,
+//  указанные в файле controls.js. Этот удар можно наносить не чаще, чем каждые 10 секунд.
--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,18 @@
-export function showWinnerModal(fighter) {
-  // call showModal function 
+import { showModal } from './modal';
+import { createElement } from '../../helpers/domHelper';
+
+export function showWinnerModal(winner) {
+  const bodyElement = createElement({ tagName: 'div', className: 'winner'});
+  const bodyText = createElement({ tagName: 'p', className: 'winner-text'});
+  const bodyHealth = createElement({ tagName: 'p', className: 'winner-health'});
+  bodyText.textContent = `${winner.name} is win!`;
+  bodyHealth.textContent = `Health is ${winner.health}`;
+  bodyElement.appendChild(bodyText);
+  bodyElement.appendChild(bodyHealth);
+
+  const modalObject = {
+    title: 'Congratulation',
+    bodyElement: bodyElement,
+  };
+  showModal(modalObject);
 }
